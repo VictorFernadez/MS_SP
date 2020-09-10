43d9c115d46214129a7dbc6a84f3d28f2e51b2f0
v94 <victor.fernandez1006@gmail.com>
2020-09-09 19:30:52 -0500

    Se agrega ConfigServer-ConfiClient para la configuracion por ambientes

--- spring-boot-servicio-item/src/main/java/com/codev/models/controllers/ItemController.java
+++ spring-boot-servicio-item/src/main/java/com/codev/models/controllers/ItemController.java
@@ -1,9 +1,17 @@
 package com.codev.models.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.core.env.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +21,16 @@ import com.codev.models.service.ItemService;
 
 @RestController
 public class ItemController {
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+	@Autowired
+	private Environment env;
+
 	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService service;
+
+	@Value("${configuracion.texto}")
+	private String texto;
 
 	@GetMapping("/listar")
 	public List<Item> listar() {
@@ -27,4 +42,16 @@ public class ItemController {
 		return service.findById(id, cantidad);
 	}
 
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+		log.info(texto);
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+	}
 }
